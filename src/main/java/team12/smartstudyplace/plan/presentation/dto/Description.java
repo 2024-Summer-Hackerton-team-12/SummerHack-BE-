29dbe7aef7aca863ee95b1f9665da9ae0c2cd2fa
GSB0203 <gimseungbin0203@gmail.com>
2024-07-18 07:58:41 +0900

    add:: 계획 관련 로직 구현

---
+++ src/main/java/team12/smartstudyplace/plan/presentation/dto/Description.java
@@ -0,0 +1,8 @@
+package team12.smartstudyplace.plan.presentation.dto;
+
+public record Description(
+    Long id,
+    String time,
+    String text
+) {
+}
---
+++ src/main/java/team12/smartstudyplace/plan/presentation/dto/PlanDescriptions.java
@@ -0,0 +1,9 @@
+package team12.smartstudyplace.plan.presentation.dto;
+
+public record PlanDescriptions(
+    Description description,
+    Description description2,
+    Description description3,
+    Description description4
+) {
+}
